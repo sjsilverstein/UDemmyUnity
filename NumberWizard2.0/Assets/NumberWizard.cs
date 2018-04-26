52edabee220650a679d099157307f7191e532f8b
Stephen Silverstein <stephenjsilverstein@gmail.com>
2018-04-26 10:42:30 -0700

    Number wizard 2.0

---
+++ NumberWizard2.0/Assets/NumberWizard.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+	int max;
+	int min;
+	int guess;	
+	int maxGuessesAllowed = 10;	
+	public Text guessText; 
+	void Start() {
+		StartGame();
+	}	
+	void StartGame(){
+		max = 1000;
+		min = 1;
+		guess = 500;
+		max = max +1;	
+	}
+	public void GuessHigher(){
+		min = guess;
+		NextGuess();
+	}
+	public void GuessLower(){
+		max = guess;
+		NextGuess();
+	}
+	void NextGuess(){
+		guess = (max+min)/2;
+		guessText.text = guess.ToString();
+		maxGuessesAllowed = maxGuessesAllowed -1;
+		if (maxGuessesAllowed <= 0 ){
+			//Go to player win screan
+			Application.LoadLevel("Win");
+		}
+	}
+}
