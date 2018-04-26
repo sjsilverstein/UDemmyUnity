62c71dfeba6a8acbad02ad362584ba856eec3650
Stephen Silverstein <stephenjsilverstein@gmail.com>
2018-04-26 16:31:21 -0700

    Added music to brick breaker

---
+++ BrickBreaker/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+	static MusicPlayer instance = null;
+	void Awake(){
+		Debug.Log ("Music player Awake " + GetInstanceID());
+		if(instance != null){
+			Destroy(gameObject);
+			Debug.Log("Duplicate music player self-desctructing!");
+		}else {
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+	}
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Music player Start " + GetInstanceID());		
+					
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ NumberWizard2.0/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name){
+		Debug.Log ("Level load request made for: "+name);
+		Application.LoadLevel(name);
+	}
+	public void QuitRequest (){
+		Debug.Log("Quit Request");
+		Application.Quit ();
+	}	
+}
